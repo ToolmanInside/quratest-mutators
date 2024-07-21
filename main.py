71f57e3eca2a6bade4062e71606b4ddbe9f1c1fb
jiaming <yejiaming20@126.com>
2024-07-21 15:54:21 +0800

    add output function

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from line import Circuit
 parser = argparse.ArgumentParser(description="Quantum Test Case Generator QuraTest")
 
 parser.add_argument("-n", type=int, default=5, help="number of qubits")
-parser.add_argument("-m", choices=['random', 'qft', 'ucnot'], help='quantum test case generator')
-parser.add_argument("-o", type=str, default='output.s', help='output path of generated test case')
+parser.add_argument("-m", default='random', choices=['random', 'qft', 'ucnot'], help='quantum test case generator')
+parser.add_argument("-o", type=str, default='output', help='output path of generated test input')
 
 args = parser.parse_args()
 
@@ -19,8 +19,7 @@ elif args.m == 'ucnot':
 
 circuit = Circuit(args.n)
 new_circuit = mutator.generate_circuit(circuit)
-# if you want to output 
 print(new_circuit.code)
-result = new_circuit.run_vec()
-print(result[0].real)
-print(result[0].imag)
+
+with open(args.o, 'w') as fi:
+    print(new_circuit.code.qasm(), file = fi)
